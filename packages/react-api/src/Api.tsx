6b4d6ed1b986f6ffa94cd6742f68224ea8c1da7e
Ben Gao <xbengao@gmail.com>
2020-07-14 19:26:44 +1200

    Revert api initializing

--- packages/react-api/src/Api.tsx
+++ packages/react-api/src/Api.tsx
@@ -5,7 +5,13 @@
 import { ApiState } from './types';
 
 import React, { useContext, useEffect, useState } from 'react';
-import { isWeb3Injected, web3Accounts, web3Enable } from '@polkadot/extension-dapp';
+import ApiPromise from '@polkadot/api/promise';
+import {
+  isWeb3Injected,
+  web3Accounts,
+  web3Enable
+} from '@polkadot/extension-dapp';
+import { WsProvider } from '@polkadot/rpc-provider';
 import { StatusContext } from '@polkadot/react-components/Status';
 import { TokenUnit } from '@polkadot/react-components/InputNumber';
 import keyring from '@polkadot/ui-keyring';
@@ -15,13 +21,13 @@ import { createType } from '@polkadot/types';
 import { formatBalance, isTestChain } from '@polkadot/util';
 import { setSS58Format } from '@polkadot/util-crypto';
 import addressDefaults from '@polkadot/util-crypto/address/defaults';
-import CENNZRuntimeTypes from '@cennznet/types/injects';
 
 import ApiContext from './ApiContext';
 import registry from './typeRegistry';
 // import {Api as ApiPromise} from '@cennznet/api';
-import ApiPromise from '@polkadot/api/promise';
+// import ApiPromise from '@polkadot/api/promise';
 import cennznetRpc from './cennznetRpcDecoration';
+import CENNZRuntimeTypes from '@cennznet/types/injects';
 
 interface Props {
   children: React.ReactNode;
@@ -47,37 +53,50 @@ let api: ApiPromise;
 
 export { api };
 
-async function loadOnReady (api: ApiPromise): Promise<State> {
-  const [properties, _systemChain, _systemName, _systemVersion, injectedAccounts] = await Promise.all([
+async function loadOnReady(api: ApiPromise): Promise<State> {
+  const [
+    properties,
+    _systemChain,
+    _systemName,
+    _systemVersion,
+    injectedAccounts
+  ] = await Promise.all([
     api.rpc.system.properties(),
     api.rpc.system.chain(),
     api.rpc.system.name(),
     api.rpc.system.version(),
     web3Accounts().then((accounts): InjectedAccountExt[] =>
-      accounts.map(({ address, meta }): InjectedAccountExt => ({
-        address,
-        meta: {
-          ...meta,
-          name: `${meta.name} (${meta.source === 'polkadot-js' ? 'extension' : meta.source})`
-        }
-      }))
+      accounts.map(
+        ({ address, meta }): InjectedAccountExt => ({
+          address,
+          meta: {
+            ...meta,
+            name: `${meta.name} (${
+              meta.source === 'polkadot-js' ? 'extension' : meta.source
+            })`
+          }
+        })
+      )
     )
   ]);
-  const ss58Format = uiSettings.prefix === -1
-    ? properties.ss58Format.unwrapOr(DEFAULT_SS58).toNumber()
-    : uiSettings.prefix;
+  const ss58Format =
+    uiSettings.prefix === -1
+      ? properties.ss58Format.unwrapOr(DEFAULT_SS58).toNumber()
+      : uiSettings.prefix;
   // const tokenSymbol = properties.tokenSymbol.unwrapOr('DEV').toString();
   const tokenSymbol = 'CPAY';
-  const tokenDecimals = properties.tokenDecimals.unwrapOr(DEFAULT_DECIMALS).toNumber();
-  const systemChain = _systemChain
-    ? _systemChain.toString()
-    : '<unknown>';
+  const tokenDecimals = properties.tokenDecimals
+    .unwrapOr(DEFAULT_DECIMALS)
+    .toNumber();
+  const systemChain = _systemChain ? _systemChain.toString() : '<unknown>';
   const isDevelopment = isTestChain(systemChain);
 
   console.log('api: found chain', systemChain, JSON.stringify(properties));
 
   // explicitly override the ss58Format as specified
-  registry.setChainProperties(createType(registry, 'ChainProperties', { ...properties, ss58Format }));
+  registry.setChainProperties(
+    createType(registry, 'ChainProperties', { ...properties, ss58Format })
+  );
 
   // FIXME This should be removed (however we have some hanging bits, e.g. vanity)
   setSS58Format(ss58Format);
@@ -90,17 +109,21 @@ async function loadOnReady (api: ApiPromise): Promise<State> {
   TokenUnit.setAbbr(tokenSymbol);
 
   // finally load the keyring
-  keyring.loadAll({
-    genesisHash: api.genesisHash,
-    isDevelopment,
-    ss58Format,
-    type: 'ed25519'
-  }, injectedAccounts);
+  keyring.loadAll(
+    {
+      genesisHash: api.genesisHash,
+      isDevelopment,
+      ss58Format,
+      type: 'ed25519'
+    },
+    injectedAccounts
+  );
 
   const defaultSection = Object.keys(api.tx)[0];
   const defaultMethod = Object.keys(api.tx[defaultSection])[0];
   const apiDefaultTx = api.tx[defaultSection][defaultMethod];
-  const apiDefaultTxSudo = (api.tx.system && api.tx.system.setCode) || apiDefaultTx;
+  const apiDefaultTxSudo =
+    (api.tx.system && api.tx.system.setCode) || apiDefaultTx;
   const isSubstrateV2 = !!Object.keys(api.consts).length;
 
   return {
@@ -115,20 +138,26 @@ async function loadOnReady (api: ApiPromise): Promise<State> {
   } as State;
 }
 
-export default function Api ({ children, url }: Props): React.ReactElement<Props> | null {
+export default function Api({
+  children,
+  url
+}: Props): React.ReactElement<Props> | null {
   const { queuePayload, queueSetTxStatus } = useContext(StatusContext);
-  const [state, setState] = useState<State>({ isApiReady: false } as Partial<State> as State);
+  const [state, setState] = useState<State>(({ isApiReady: false } as Partial<
+    State
+  >) as State);
   const [isApiConnected, setIsApiConnected] = useState(false);
   const [isWaitingInjected, setIsWaitingInjected] = useState(isWeb3Injected);
   const [isInitialized, setIsInitialized] = useState(false);
 
   // initial initialization
   useEffect((): void => {
+    const provider = new WsProvider(url);
     const signer = new ApiSigner(queuePayload, queueSetTxStatus);
 
     // api = new ApiPromise({ provider: url, registry, signer });
     api = new ApiPromise({
-      // provider,
+      provider,
       registry,
       rpc: cennznetRpc,
       signer,
@@ -137,13 +166,16 @@ export default function Api ({ children, url }: Props): React.ReactElement<Props
 
     api.on('connected', (): void => setIsApiConnected(true));
     api.on('disconnected', (): void => setIsApiConnected(false));
-    api.on('ready', async (): Promise<void> => {
-      try {
-        setState(await loadOnReady(api));
-      } catch (error) {
-        console.error('Unable to load chain', error);
+    api.on(
+      'ready',
+      async (): Promise<void> => {
+        try {
+          setState(await loadOnReady(api));
+        } catch (error) {
+          console.error('Unable to load chain', error);
+        }
       }
-    });
+    );
 
     injectedPromise
       .then((): void => setIsWaitingInjected(false))
@@ -157,7 +189,9 @@ export default function Api ({ children, url }: Props): React.ReactElement<Props
   }
 
   return (
-    <ApiContext.Provider value={{ ...state, api, isApiConnected, isWaitingInjected }}>
+    <ApiContext.Provider
+      value={{ ...state, api, isApiConnected, isWaitingInjected }}
+    >
       {children}
     </ApiContext.Provider>
   );
