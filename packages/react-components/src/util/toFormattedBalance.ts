020df99864cc22adbbd1872762b310a0707dab6b
Ben Gao <xbengao@gmail.com>
2020-08-14 13:42:27 +1200

    Refactor toFormattedBalance by using BN

--- packages/react-components/src/util/toFormattedBalance.ts
+++ packages/react-components/src/util/toFormattedBalance.ts
@@ -1,7 +1,8 @@
+import BN from 'bn.js';
 import { formatBalance } from '@polkadot/util';
 
 interface Arguments {
-  balance: string;
+  balance: BN | string;
   fixedPoint?: number;
   unit?: string;
 }
@@ -24,20 +25,17 @@ const toFormattedBalance = (args: Arguments): string => {
     fixedPoint = DEFAULT_FIXED_POINT,
     unit = DEFAULT_UNIT
   } = args;
-  const balanceAsNumber = parseInt(balance, 10);
   const unitPart = unit ? ` ${unit}` : '';
-
-  if (!balance || isNaN(balanceAsNumber)) {
-    throw new Error(`Balance value is not valid: ${balance}`);
-  }
 
   /**
    * Condition 1: balance length is smaller than fixed point, e.g:
    * "123" ==> "0.1230" # when value length (3) is smaller than fixed point (4)
    */
-  if (balance.length <= fixedPoint) {
+  if (balance.toString().length <= fixedPoint) {
+    const valueAsBN = BN.isBN(balance) ? balance : new BN(balance);
     const scalingSize = Math.pow(10, 1 - fixedPoint);
-    const valuePart = (balanceAsNumber * scalingSize).toFixed(fixedPoint);
+    const valuePart = (valueAsBN.toNumber() * scalingSize).toFixed(fixedPoint);
+
 
     return `${valuePart}${unitPart}`;
   }
@@ -53,9 +51,8 @@ const toFormattedBalance = (args: Arguments): string => {
   };
   const formattedBalance = formatBalance(balance, polkadotFormatBalanceOptions);
 
-  console.log('object', polkadotFormatBalanceOptions);
   const integerPart = formattedBalance.split('.')[0];
-  const decimalPart = balance.slice(-fixedPoint);
+  const decimalPart = balance.toString().slice(-fixedPoint);
 
   return `${integerPart}.${decimalPart}${unitPart}`;
 };
