b26a7b7334ba1eb3e29fe4d45e45a574b3131a2c
KarishmaBothara <bothara.karishma@gmail.com>
2020-12-10 13:15:50 +1300

    fixing babel config for jest

--- babel.config.js
+++ babel.config.js
@@ -1,1 +1,29 @@
-module.exports = require('@polkadot/dev/config/babel');
+// Copyright 2017-2020 @polkadot/dev authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+module.exports = {
+    presets: [
+        ['@babel/preset-env', {
+            modules: 'commonjs',
+            targets: {
+                browsers: '>0.25% and last 2 versions and not ie 11 and not OperaMini all',
+                node: '10'
+            }
+        }],
+        '@babel/preset-typescript',
+        '@babel/preset-react'
+    ],
+    plugins: [
+        // ordering important, decorators before class properties
+        ['@babel/plugin-proposal-decorators', { legacy: true }],
+        ['@babel/plugin-proposal-class-properties', { loose: true }],
+        ['@babel/plugin-proposal-pipeline-operator', { proposal: 'fsharp' }],
+        '@babel/plugin-proposal-numeric-separator',
+        '@babel/plugin-proposal-object-rest-spread',
+        '@babel/plugin-proposal-optional-chaining',
+        '@babel/plugin-transform-runtime',
+        '@babel/plugin-syntax-dynamic-import',
+        'babel-plugin-styled-components',
+    ].filter((p) => !!p)
+};
